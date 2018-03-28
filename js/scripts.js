ab68375ec361883d913f1ec9081243acd927aeaa
Janice <janicejoonam@gmail.com>
2018-03-28 14:36:56 -0700

    add prototype totalCost

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,13 @@
 //Business Logic
-function Pizza(size, protein, veggies) {
+function Pizza(size, protein, veggies, total) {
   this.size = size
   this.protein = protein
   this.veggies = veggies
+  this.total = [];
 }
 
-Pizza.prototype.chooseProtein = function(protein) {
-  this.protein.push(protein);
-}
 
-Pizza.prototype.chooseVeggies = function(veggies) {
-  this.veggies.push(veggies);
-}
-
-Pizza.prototype.totalCost = function() {
+Pizza.prototype.chooseSize = function() {
   var cost = 0;
   if (this.size === "small") {
     cost = 5;
@@ -25,31 +19,51 @@ Pizza.prototype.totalCost = function() {
     cost = 15;
   }
 
-  this.protein.forEach(function() {
-    cost += 0.50;
+  Pizza.prototype.chooseProtein = function(protein) {
+
+    this.protein.forEach(function() {
+      cost += 0.50;
+    });
+  }
+
+  Pizza.prototype.chooseVeggies = function(veggies) {
+
+    this.veggies.forEach(function() {
+      cost += 0.30;
+    });
+  }
+
+Pizza.prototype.totalCost = function() {
+  var totalCost = 0;
+  this.total.forEach(function() {
+    totalCost += Pizza.cost;
   });
-  this.veggies.forEach(function() {
-    cost+= 0.30;
-  });
-  this.cost = cost;
+  this.totalCost = totalCost;
+  return newPizza.totalCost;
 }
+
+
+
+
+
+
 
 //User Interface Logic
 $(document).ready(function() {
   $("form#submit").submit(function(event) {
     event.preventDefault();
+      var newPizza = new Pizza (size, protein, veggies, total);
+      $("input[name='size']:checked").each(function() {
+        newPizza.addSize($(this).val());
+      $("input[name='protein']:checked").each(function() {
+        newPizza.addProtein($(this).val());
+      $("input[name='veggies']:checked").each(function() {
+        newPizza.addVeggies($(this).val());
+      });
 
-    var size = $("input[name="size"]:checked").val();
-    var protein = $("input[name="protein"]:checked").val();
-    var veggies = $("input[name="veggies"]:checked").val();
-
-      var newPizza = new Pizza (size, protein, veggies);
-      newPizza.addProtein($(this).val());
-      newPizza.addVeggies($(this).val());
       $("#finalcost").text(newPizza.totalCost);
-      newPizza.totalCost();
-      $(".cost").show();
-      debugger;
-
+      $("#cost").show();
+      $("#orderpizza").hide();
+    });
   });
 });
