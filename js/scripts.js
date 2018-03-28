06edae71fe26ac55021586991d0abed7e018b404
Janice <janicejoonam@gmail.com>
2018-03-28 16:47:42 -0700

    add readme.md file

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 //Business Logic
+
+//Constructor is Pizza
 function Pizza(size, protein, veggies, total) {
   this.size = size
   this.protein = protein
@@ -6,7 +8,7 @@ function Pizza(size, protein, veggies, total) {
   this.total = [];
 }
 
-
+//4 Prototypes: size, protein, veggies, and total cost
 Pizza.prototype.chooseSize = function() {
   var cost = 0;
   if (this.size === "small") {
@@ -19,19 +21,22 @@ Pizza.prototype.chooseSize = function() {
     cost = 15;
   }
 
-  Pizza.prototype.chooseProtein = function(protein) {
+Pizza.prototype.chooseProtein = function(protein) {
 
     this.protein.forEach(function() {
       cost += 0.50;
     });
-  }
 
-  Pizza.prototype.chooseVeggies = function(veggies) {
+  this.protein.forEach(function() {
+    cost += 0.50;
+  });
+}
 
-    this.veggies.forEach(function() {
-      cost += 0.30;
-    });
-  }
+Pizza.prototype.chooseVeggies = function(veggies) {
+  this.veggies.forEach(function() {
+    cost += 0.30;
+  });
+}
 
 Pizza.prototype.totalCost = function() {
   var totalCost = 0;
@@ -52,6 +57,7 @@ Pizza.prototype.totalCost = function() {
 $(document).ready(function() {
   $("form#submit").submit(function(event) {
     event.preventDefault();
+    //instance
       var newPizza = new Pizza (size, protein, veggies, total);
       $("input[name='size']:checked").each(function() {
         newPizza.addSize($(this).val());
@@ -64,6 +70,7 @@ $(document).ready(function() {
       $("#finalcost").text(newPizza.totalCost);
       $("#cost").show();
       $("#orderpizza").hide();
+      });
     });
   });
 });
---
+++ readme.md
@@ -0,0 +1,39 @@
+<h1>Project Name: Object Oriented JavaScript Project</h2>
+<h2>Order Pizza By: Janice Nam</h2>
+<h2>Description</h2>
+This is Janice's first pizza generator!
+Create a website for a pizza company where a user can choose the size of the pizza, add one or more individual toppings (cheese, pepperoni, olives, etc.), and see the final cost.
+<ul>
+<li>Allow the user to choose toppings and size for the pizza they'd like to order.</li>
+<li>Create a pizza object constructor with properties for toppings and size.</li>
+<li>Create a prototype method for the cost of a pizza depending on the selections chosen.</li>
+</ul>
+
+<h2>Setup/Installation Requirements</h2>
+<p>*Open web browser</p>
+<p>*Open web page</p>
+<ul>
+<li>This is a simple webpage. You shouldn't have any issues with this.</li>
+<li>Link to site on GitHub pages: https://github.com/janicejoonam/pizza </li>
+</ul>
+<p>*Open Terminal</p>
+<p>*In Terminal, enter the following:</p>
+<ul>
+<li>git clone https://github.com/janicejoonam/pizza.git</li>
+<li>cd pizza</li>
+</ul>
+<p>*Then, open the pizza folder on your Desktop and open the index.html file</p>
+<p>*This will open the webpage on your web browser</p>
+
+<h2>Known Bugs</h2>
+*Will not submit results
+
+<h2>Support and Contact Details</h2>
+Please contact the contributor for any questions or concerns.
+
+<h2>Technologies Used</h2>
+HTMl, CSS, Bootstrap, JavaScript, and jquery.
+
+<h2>License</h2>
+This software is licensed under the MIT license
+Copyright (c) 2018 Janice Nam
