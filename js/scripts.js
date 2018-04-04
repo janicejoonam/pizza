f30cd38eb93cdbbf88fb0d9be5670270d8101137
Janice <janicejoonam@gmail.com>
2018-04-04 13:01:34 -0700

    add renamed prototypes to sizeCost and giving user only 1 option for size of pizza by adding select drop down instead of checkbox in html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-//Business Logic
+//~~B U S I N E S S   L O G I C~~
 
 //Constructor is Pizza
 function Pizza(size, toppings, price) {
@@ -7,16 +7,16 @@ function Pizza(size, toppings, price) {
   this.price = price;
 }
 
-//Prototype is cost
-Pizza.prototype.pricePrototype = function(size, toppingsArray) {
+//Prototype is sizeCost
+Pizza.prototype.sizeCost = function(size, toppingsArray) {
   var total = 0;
-  if (size === "small") {
+  if (size === "Small 10-inch") {
     total += 5.00;
-  } else if (size === "medium") {
+  } else if (size === "Medium 12-inch") {
     total += 8.00;
-  } else if (size === "large") {
+  } else if (size === "Large 14-inch") {
     total += 12.00;
-  } else if (size === "extralarge") {
+  } else if (size === "Extra-Large 16-inch") {
     total += 15.00;
   }
 
@@ -25,14 +25,14 @@ Pizza.prototype.pricePrototype = function(size, toppingsArray) {
     this.price = total;
   } else {
     for (var i = 0; i < toppingsArray.length; i++) {
-      total += 0.50;
+      total += 1.00;
       this.price = total;
     }
   }
 }
 
 Pizza.prototype.cost = function(size, toppings, price) {
-  return "$" + this.price + " " + this.size + " pizza, with " + this.toppings + ".";
+  return "Your total is $" + this.price + " for a " + this.size + " pizza with " + this.toppings + ".";
 }
 
 
@@ -41,10 +41,10 @@ Pizza.prototype.cost = function(size, toppings, price) {
 //User Interface Logic
 $(document).ready(function() {
 
-  $("form#orderPizza").submit(function(event) {
+  $("#orderPizza").submit(function(event) {
     event.preventDefault();
 
-    var pizzaSize = $(".size").val();
+    var sizeNew = $("#pizza-size").val();
     var toppings = $("input:checkbox[name=toppings]:checked");
     var toppingsArray = [];
     for (var i = 0; i < toppings.length; i++) {
@@ -52,25 +52,14 @@ $(document).ready(function() {
     }
 
     //Instance
-    var newPizza = new Pizza (pizzaSize, toppingsArray, price);
-    var pizzaPrice = newPizza.pricePrototype (pizzaSize, toppingsArray);
-    var pizzaCost = newPizza.cost (pizzaSize, toppingsArray, price);
+    var newPizza = new Pizza (sizeNew, toppingsArray, price);
+    var price = newPizza.sizeCost (sizeNew, toppingsArray);
+    var output = newPizza.cost (sizeNew, toppingsArray, price);
 
     //Hides 1st page, shows second page with results
-    $("button#submit1").on("click", function(event) {
-      $("#page1").hide();
-      $("#page2").show();
-      event.preventDefault();
-    });
-
-    //Order Page
-    $("#size").text(pizzaSize);
-    $("#price").text(pizzaCost);
-    $("#toppings").text("");
-    $("input:checkbox[name=toppings]:checked").each(function() {
-      var selectedToppings = $(this).val();
-      $("#toppings").append(selectedToppings + "<br>");
-    });
+    $("#page2").show();
+    $("#page1").hide();
+    $("#order").text(output);
   });
 });
 
